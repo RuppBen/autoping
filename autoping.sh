49afb48c5f343d68135e436ad8b06911c974ad74
root <admin@example.com>
2020-10-01 13:15:24 +0200

    Weird solution without loops

--- autoping.sh
+++ autoping.sh
@@ -1,20 +1,70 @@
-#JUMPDESTINY=""
-#JUMPSERVER=""
+#!/bin/bash
 
-TESTREACH1="192.168.6.160"
-TESTREACH2="192.168.6.161"
-TESTREACH3="192.168.6.162"
-TESTREACH4="192.168.6.163"
-TESTREACH5="192.168.6.164"
+#Insert IP Address of Jumpserver
+JUMPSERVER=""
+#Insert IP Address of Destinationserver
+JUMPDESTINY1=""
+JUMPDESTINY2=""
+JUMPDESTINY3=""
+JUMPDESTINY4=""
+JUMPDESTINY5=""
+#Insert IP Address of Server to ping
+TESTREACH1=""
+TESTREACH2=""
+TESTREACH3=""
+TESTREACH4=""
+TESTREACH5=""
 
-#ssh root@$JUMPSERVER
-#ssh root@$JUMPDESTINY
+ssh root@$JUMPSERVER
 
-ping -q -c 3 $TESTREACH1
-ping -q -c 3 $TESTREACH2
-ping -q -c 3 $TESTREACH3
-ping -q -c 3 $TESTREACH4
-ping -q -c 3 $TESTREACH5
+	ssh $JUMPDESTINY1
+
+		ping -q -c 3 $TESTREACH1
+		ping -q -c 3 $TESTREACH2
+		ping -q -c 3 $TESTREACH3
+		ping -q -c 3 $TESTREACH4
+		ping -q -c 3 $TESTREACH5
+
+	exit
+
+	ssh $JUMPDESTINY2
+
+		ping -q -c 3 $TESTREACH1
+		ping -q -c 3 $TESTREACH2
+		ping -q -c 3 $TESTREACH3
+		ping -q -c 3 $TESTREACH4
+		ping -q -c 3 $TESTREACH5
+
+	exit
+
+	ssh $JUMPDESTINY3
+
+		ping -q -c 3 $TESTREACH1
+                ping -q -c 3 $TESTREACH2
+                ping -q -c 3 $TESTREACH3
+                ping -q -c 3 $TESTREACH4
+                ping -q -c 3 $TESTREACH5
+
+	exit
+
+	ssh $JUMPDESTINY4
+
+		ping -q -c 3 $TESTREACH1
+                ping -q -c 3 $TESTREACH2
+                ping -q -c 3 $TESTREACH3
+                ping -q -c 3 $TESTREACH4
+                ping -q -c 3 $TESTREACH5
 
 
-#exit
+	exit
+
+	ssh $JUMPDESTINY5
+
+		ping -q -c 3 $TESTREACH1
+                ping -q -c 3 $TESTREACH2
+                ping -q -c 3 $TESTREACH3
+                ping -q -c 3 $TESTREACH4
+                ping -q -c 3 $TESTREACH5
+
+	exit
+exit
