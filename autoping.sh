346a8263164fa95da85630e3b750a5351848b176
root <admin@example.com>
2020-10-01 14:01:13 +0200

    Rewrote code to loop, not testet

--- autoping.sh
+++ autoping.sh
@@ -8,6 +8,7 @@ JUMPDESTINY2=""
 JUMPDESTINY3=""
 JUMPDESTINY4=""
 JUMPDESTINY5=""
+DUMMY=$JUMPDESTINY1
 #Insert IP Address of Server to ping
 TESTREACH1=""
 TESTREACH2=""
@@ -17,27 +18,9 @@ TESTREACH5=""
 
 ssh root@$JUMPSERVER
 
-	ssh $JUMPDESTINY1
-
-		ping -q -c 3 $TESTREACH1
-		ping -q -c 3 $TESTREACH2
-		ping -q -c 3 $TESTREACH3
-		ping -q -c 3 $TESTREACH4
-		ping -q -c 3 $TESTREACH5
-
-	exit
-
-	ssh $JUMPDESTINY2
-
-		ping -q -c 3 $TESTREACH1
-		ping -q -c 3 $TESTREACH2
-		ping -q -c 3 $TESTREACH3
-		ping -q -c 3 $TESTREACH4
-		ping -q -c 3 $TESTREACH5
-
-	exit
-
-	ssh $JUMPDESTINY3
+	for (( a=1; a<5; a++ ))
+	do
+		ssh $DUMMY
 
 		ping -q -c 3 $TESTREACH1
                 ping -q -c 3 $TESTREACH2
@@ -45,26 +28,25 @@ ssh root@$JUMPSERVER
                 ping -q -c 3 $TESTREACH4
                 ping -q -c 3 $TESTREACH5
 
-	exit
+		exit
 
-	ssh $JUMPDESTINY4
+		if (( a=1 ))
+		then
+			$DUMMY=$JUMPDESTINY2
 
-		ping -q -c 3 $TESTREACH1
-                ping -q -c 3 $TESTREACH2
-                ping -q -c 3 $TESTREACH3
-                ping -q -c 3 $TESTREACH4
-                ping -q -c 3 $TESTREACH5
+		elif (( a=2 ))
+		then
+			$DUMMY=$JUMPDESTINY3
 
+		elif (( a=3 ))
+		then
+			$DUMMY=$JUMPDESTINY4
 
-	exit
-
-	ssh $JUMPDESTINY5
-
-		ping -q -c 3 $TESTREACH1
-                ping -q -c 3 $TESTREACH2
-                ping -q -c 3 $TESTREACH3
-                ping -q -c 3 $TESTREACH4
-                ping -q -c 3 $TESTREACH5
-
-	exit
+		elif (( a=4 ))
+		then
+			$DUMMY=$JUMPDESTINY5
+		else
+			echo "panik, panik a lot"
+		fi
+	done
 exit
